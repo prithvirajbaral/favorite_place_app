4a2cb165f45a92cc371479dca041ad003001c099
prithvirajbaral <prithvirajbaral@gmail.com>
2025-02-17 15:01:17 +0530

    added the generic type

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -11,4 +11,4 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
 }
 
 
-final userPlacesProvider = StateNotifierProvider((ref) => UserPlacesNotifier());
+final userPlacesProvider = StateNotifierProvider<UserPlacesNotifier, List<Place>>((ref) => UserPlacesNotifier());
