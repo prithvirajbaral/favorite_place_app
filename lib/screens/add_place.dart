06f95a993717d1baa6b558f931242e536fddef30
prithvirajbaral <prithvirajbaral@gmail.com>
2025-02-17 17:09:47 +0530

    add LocationInput widget to AddPlaceScreen for location selection

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:favorite_place_app/providers/user_places.dart';
 import 'package:favorite_place_app/widgets/image_input.dart';
+import 'package:favorite_place_app/widgets/location_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -60,6 +61,10 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
                 _selectedImage = image;
               },
             ),
+            const SizedBox(
+              height: 16,
+            ),
+            LocationInput(),
             const SizedBox(
               height: 16,
             ),
