f7beede6644ce541f9ed5ca038a3ab4761206505
prithvirajbaral <prithvirajbaral@gmail.com>
2025-02-17 16:52:05 +0530

    enhance UI by adding padding to PlacesList and displaying images in PlacesDetail and PlacesList

--- lib/screens/places_detail.dart
+++ lib/screens/places_detail.dart
@@ -12,13 +12,15 @@ class PlacesDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(place.title),
       ),
-      body: Center(
-        child: Text(
-          place.title,
-          style: TextStyle(
-            color: Theme.of(context).colorScheme.onSurface,
-          ),
-        ),
+      body: Stack(
+        children: [
+          Image.file(
+            place.image,
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          )
+        ],
       ),
     );
   }
