270612ce2a437713096bc629b136799c48fe5f78
prithvirajbaral <prithvirajbaral@gmail.com>
2025-02-17 15:21:31 +0530

    add PlacesDetailScreen to display details of a selected place

---
+++ lib/screens/places_detail.dart
@@ -0,0 +1,25 @@
+import 'package:favorite_place_app/models/place.dart';
+import 'package:flutter/material.dart';
+
+class PlacesDetailScreen extends StatelessWidget {
+  const PlacesDetailScreen({super.key, required this.place});
+
+  final Place place;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(place.title),
+      ),
+      body: Center(
+        child: Text(
+          place.title,
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.onSurface,
+          ),
+        ),
+      ),
+    );
+  }
+}
